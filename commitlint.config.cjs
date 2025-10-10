4353ad8a8753f82d586cc9ecbdcce4c392928c47
Rat-financial <723josha@gmail.com>
2025-10-11 03:25:33 +0800

    docs: to zh_tw

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,14 +1,14 @@
 /**
- * build 编译相关的修改，例如发布版本、对项目构建或者依赖的改动
- * chore 其他修改, 比如改变构建流程、或者增加依赖库、工具等
- * docs 文档修改
- * feat 新特性、新功能
+ * build 編譯相關的修改，例如發布版本、對專案建置或依賴的改動
+ * chore 其他修改, 例如改變建置流程、或增加依賴函式庫、工具等
+ * docs 文件修改
+ * feat 新功能、新功能
  * fix 修改bug
- * perf 优化相关，比如提升性能、体验
- * refactor 代码重构
- * revert 回滚到上一个版本
- * style 代码格式修改
- * test 测试用例修改
+ * perf 優化相關，例如提升效能、體驗
+ * refactor 程式碼重構
+ * revert 回滾到上一個版本
+ * style 程式碼格式修改
+ * test 測試用例修改
  */
 /* eslint-env node */
 module.exports = {
